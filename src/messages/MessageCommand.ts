aa28a87fede004af405f812710b5090ace6b875c
Tannatee Juchan <ju.tannatee2543@gmail.com>
2022-03-26 03:34:25 +0700

    add: manager logic

---
+++ src/messages/MessageCommand.ts
@@ -0,0 +1,11 @@
+/* eslint-disable semi */
+/* eslint-disable no-extra-semi */
+import { Message } from 'discord.js';
+
+export type MessageCommandInfo = string;
+
+export default interface MessageCommand {
+  getInfo(): MessageCommandInfo;
+
+  execute(commandInfo: string, split: string[], message: Message): Promise<void>;
+};
--- src/messages/MessageManager.ts
+++ src/messages/MessageManager.ts
@@ -1,14 +1,45 @@
-import { Client } from 'discord.js';
+import MessageCommand from '@src/messages/MessageCommand';
+import { Client, Message } from 'discord.js';
 
 export class MessageManager {
 
+  protected readonly client: Client;
   protected readonly prefix: string;
 
-  protected readonly client: Client;
+  private messageLabel = new Map<string, MessageCommand>();
 
   public constructor(client: Client, prefix: string) {
-    this.prefix = prefix;
     this.client = client;
+    this.prefix = prefix;
+  }
+
+  public async registerMessage(messages: MessageCommand[]): Promise<void> {
+    const messagesInfo = messages.map((message) => message.getInfo());
+
+    messages.forEach((message) => {
+      this.messageLabel.set(message.getInfo(), message);
+    });
+
+    console.info(`Message Commands: ${messagesInfo.join(', ')}`);
+    console.info(`Registered ${messages.length} MessageCommands successfully`);
+  }
+
+  public async executeMessage(message: Message): Promise<void> {
+    if (!message.content.startsWith(this.prefix)) return;
+
+    const split: string[] = message.content.split(' ');
+    if (split.length === 0) return;
+
+    const commandExcludePrefix = split[0].substring(this.prefix.length);
+    const command = this.messageLabel.get(commandExcludePrefix);
+
+    if (!command) return;
+
+    try {
+      command.execute(commandExcludePrefix, split, message);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
 }

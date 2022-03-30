754115297d3ee2a63770d1e32458ba18f7145024
Tannatee Juchan <ju.tannatee2543@gmail.com>
2022-03-30 22:47:51 +0700

    refactor: change file name

--- src/__loader__/DiscordBot_Loader.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import DiscordBot from '@src/main.discordbot';
-
-const { TOKEN } = process.env;
-
-new DiscordBot(TOKEN).startBot();
--- src/commands/audio/info/instance.discordbot.ts
+++
@@ -1,6 +0,0 @@
-import TrackPlayerManager from '@src/commands/audio/track/playermanager.track';
-
-/* eslint-disable semi */
-export default interface BotInstance {
-  getTrackPlayerManager(): TrackPlayerManager;
-}
